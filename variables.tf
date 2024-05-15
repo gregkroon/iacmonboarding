e047d787447fb42e5eaa485abc3e3dcd35028818
gregkroon <44827446+gregkroon@users.noreply.github.com>
2024-05-16 09:14:54 +1000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -18,3 +18,34 @@ variable "HARNESS_WORKSPACE_ID" {
  type        = string
  }
 
+
+variable "HARNESS_COST_ESTIMATION_ENABLED" {
+ type        = string
+ }
+
+variable "HARNESS_PROVISIONER_VERSION" {
+ type        = string
+ }
+
+variable "HARNESS_REPO_PATH" {
+ type        = string
+ }
+
+variable "HARNESS_REPO" {
+ type        = string
+ }
+
+variable "HARNESS_PROVIDER_CONNECTOR" {
+ type        = string
+ }
+
+variable "HARNESS_PROVISONER_TYPE" {
+ type        = string
+ }
+
+variable "HARNESS_REPO_CONNECTOR" {
+ type        = string
+ }
+
+
+ 
