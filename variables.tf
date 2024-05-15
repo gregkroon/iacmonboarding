b5b92c0ff279867c2c577be1631a92e6094f6497
gregkroon <44827446+gregkroon@users.noreply.github.com>
2024-05-16 08:14:07 +1000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "HARNESS_API_KEY" {
+ type        = string
+ }
+
+variable "HARNESS_ACCOUNT_ID" {
+ type        = string
+ }
+
+variable "HARNESS_PROJECT_ID" {
+ type        = string
+ }
+
+variable "HARNESS_ORG_ID" {
+ type        = string
+ }
