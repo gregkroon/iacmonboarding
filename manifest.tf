8582d8734fc6de49cad79d957f2fe3bbf11d97ac
gregkroon <44827446+gregkroon@users.noreply.github.com>
2024-05-16 15:42:29 +1000

    Update manifest.tf

--- manifest.tf
+++ manifest.tf
@@ -120,3 +120,45 @@ depends_on = [harness_platform_connector_github.github_connector,harness_platfor
 
 }
 
+resource "harness_platform_pipeline" "pipeline" {
+  org_id     = var.HARNESS_ORG_ID
+  project_id = var.HARNESS_PROJECT_ID
+  identifier = var.HARNESS_PIPELINE_ID
+  name       = var.HARNESS_PIPELINE_ID
+
+  stage {
+    name       = "IACM Stage"
+    identifier = "IACM_STAGE"
+    type       = "IACM"
+
+    spec {
+      infrastructure {
+        type = "Workspace"
+        spec {
+          workspace_ref = harness_platform_workspace.workspace.id
+        }
+      }
+
+      execution {
+        steps {
+          step {
+            name       = "Apply Terraform"
+            identifier = "APPLY_TERRAFORM"
+            type       = "TerraformApply"
+            spec {
+              configuration {
+                secret_id = "AWS_SECRET_KEY"
+                access_key = var.AWS_ACCESS_KEY
+                branch = var.HARNESS_REPO_BRANCH
+                repo = var.HARNESS_REPO
+                path = var.HARNESS_REPO_PATH
+                connector_ref = var.HARNESS_GITHUB_CONNECTOR_ID
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
